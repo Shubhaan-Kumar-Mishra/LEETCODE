41a3a30f8cf2cea9bb96eb8af2a99a3ccf9234df
Shubhaan-Kumar-Mishra <mishrashubhaankumar@gmail.com>
2026-07-06 10:38:18 +0530

    Time: 1 ms (100%), Space: 45.5 MB (30.03%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,26 +1,26 @@
+// TRIAL ONLY NOT FOR USE
 
 class Solution {
-    long resolves(long a, long b, char Operator) {
-        if (Operator == '+') return a + b;
-        else if (Operator == '-') return a - b;
-        else if (Operator == '*') return a * b;
-        return a / b;
-    }
-
+    int idx;
     public int evalRPN(String[] tokens) {
-        Stack<Long> stack = new Stack<>();
-        int n = tokens.length;
-        for (int i = 0; i < n; i++) {
-            if (tokens[i].length() == 1 && tokens[i].charAt(0) < 48) {
-                long integer2 = stack.pop();
-                long integer1 = stack.pop();
-                char operator = tokens[i].charAt(0);
-                long resolvedAns = resolves(integer1, integer2, operator);
-                stack.push(resolvedAns);
-            } else {
-                stack.push(Long.parseLong(tokens[i]));
+        idx = tokens.length -1;
+        return help(tokens);
+    }
+    public int help(String[] s){
+        return switch(s[idx--]){
+            case "+" -> help(s) + help(s);
+            case "*" -> help(s) * help(s);
+
+            case "-" ->{
+            int a = help(s);
+            yield help(s) - a;
             }
-        }
-        return stack.pop().intValue();
+
+            case "/" ->{
+            int a = help(s);
+            yield help(s)/a;
+            }
+            default -> Integer.parseInt(s[idx+1]);
+        };
     }
 }
